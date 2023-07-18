ed3acbdae26177eceba4150d62141bbc096552cb
Matheus Geiger <matheus.slgd@gmail.com>
2023-07-18 14:34:03 -0300

    fix(django-stomp): included check for None destination

--- autodynatrace/wrappers/django_stomp/utils.py
+++ autodynatrace/wrappers/django_stomp/utils.py
@@ -1,12 +1,14 @@
+from typing import Optional
+
 import oneagent
 
 
-def get_messaging_type_by_queue_name(queue_name: str) -> int:
+def get_messaging_type_by_queue_name(queue_name: Optional[str] = None) -> int:
     """Helper function to get queue type by name. If 'VirtualTopic' exists in the destination name
     this queue is a TOPIC type.
     By default the type of queue is QUEUE.
     """
-    if "VirtualTopic" in queue_name:
+    if queue_name and "VirtualTopic" in queue_name:
         return oneagent.common.MessagingDestinationType.TOPIC
 
     return oneagent.common.MessagingDestinationType.QUEUE
