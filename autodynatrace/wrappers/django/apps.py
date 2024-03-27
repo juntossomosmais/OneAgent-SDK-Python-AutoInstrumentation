ca998b0c89f15e14e725394ea8626923b784b754
Alexandre Xavier <ale.bxsantos@gmail.com>
2024-03-27 15:27:33 -0300

    feat: adds oneagent initialization at django app ready stage

--- autodynatrace/wrappers/django/apps.py
+++ autodynatrace/wrappers/django/apps.py
@@ -8,4 +8,8 @@ class DynatraceConfig(AppConfig):
     label = "dynatrace_django"
 
     def ready(self):
+        import oneagent
+        sdk_options = oneagent.sdkopts_from_commandline(remove=True)
+        is_forkable_enabled = bool(strtobool(os.getenv("AUTODYNATRACE_FORKABLE", "True")))
+        oneagent.initialize(sdk_options, forkable=is_forkable_enabled)
         instrument_django()
